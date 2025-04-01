ff323b7b50a3d96301974647b35861ef402ad3de
W0lfcloud <misanavrnt@gmail.com>
2025-04-01 22:57:37 +0200

    Update

--- Main/Skripty/StoryScript.js
+++ Main/Skripty/StoryScript.js
@@ -1,4 +1,4 @@
-// d:\IT\ProjektDungeon\tweego-2.1.1-windows-x64\tweego.exe -o index.html Main Imgs -w 
+// d:\IT\_hry\ProjektDungeon\tweego-2.1.1-windows-x64\tweego.exe -o index.html Main Imgs -w
 Config.macros.typeVisitedPassages = false;
 
 
@@ -12,3 +12,11 @@ Setting.addToggle("fullscreen", {
 	label : "Zapnout fullscreen",
   	onChange : zapnoutFullscreen
 }); 
+
+l10nStrings.settingsTitle = 'Nastavení hry';
+l10nStrings.textOff   = 'Vyp';
+l10nStrings.textOn    = 'Zap';
+l10nStrings.settingsTextReset = 'Resetovat nastavení';
+//TODO: Opravit nastavení :(
+// Nejaky ty ilustrace
+// Nejaky ty zvuky?? 
